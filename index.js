0e23013ebd0b0cee3e0f4244d119edac09f70d42
reuben <reuben.eggar@me.com>
2017-02-01 22:20:43 +0000

    🐛 Handle Negative Numbers

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 module.exports = (number, digits) => {
-  const half = Math.pow(10, (digits - Math.floor(Math.log(number) / Math.LN10) - 1))
+  const half = Math.pow(10, (digits - Math.floor(Math.log(Math.abs(number)) / Math.LN10) - 1))
   return Math.round(number * half) / half
 }

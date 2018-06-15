2a5e3ac2d33bec033080dff5ea3041b3120d7539
Reuben <reuben.eggar@me.com>
2018-06-15 12:45:08 +0100

    ✨ Standard Form Option

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
-module.exports = (number, digits) => {
-  const half = Math.pow(10, (digits - Math.floor(Math.log(Math.abs(number)) / Math.LN10) - 1))
-  return Math.round(number * half) / half
+module.exports = (number, digits=4, max=4) => {
+const half = Math.pow(10, (digits - Math.floor(Math.log(Math.abs(number)) / Math.LN10) - 1))
+const atFig = Math.round(number * half) / half
+
+if(Math.ceil(Math.log10(atFig)) <= max) return atFig
+const [first, second] = atFig.toExponential(digits-1).split('e')
+return `${first}x10${[...second].map(d => ('⁰¹²³⁴⁵⁶⁷⁸⁹'[d]) || (d === '-' ? '⁻' : '')).join('')}`
 }
